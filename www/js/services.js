a767ec96478bfddc332dd4e908ea71bd3fabcbdb
Andrew Welton <awelton@mail.uoguelph.ca>
2015-11-12 13:05:36 -0500

    Roles should now be functional

--- www/js/services.js
+++ www/js/services.js
@@ -6,14 +6,23 @@ angular.module("starter")
 
   function login(name, pw) {
     username = name;
+    if(name.toLowerCase() == "admin") {
+      role = "admin";
+    } else if(name.toLowerCase() == "captain") {
+      role = "captain";
+    } else {
+      role = "participant";
+    }
     isAuthenticated = true;
     window.localStorage.setItem("loggedIn", true);
+    window.localStorage.setItem("role", role);
   }
   function logout() {
     username = "";
     isAuthenticated = false;
     role = "";
     window.localStorage.setItem("loggedIn", false);
+    window.localStorage.setItem("role", "");
   }
   return {
     login: login,
@@ -27,6 +36,13 @@ angular.module("starter")
         }
       }
       return isAuthenticated;
+    },
+    role: function() {
+      if(!isAuthenticated()) {
+        return;
+      }
+      role = window.localStorage.getItem("role");
+      return role;
     }
   }
 })
